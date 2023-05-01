26014475f2a33b7f3380f389b5a1a844ef4da07f
Stipe Stanic <stipe.stanic156@gmail.com>
2023-05-01 23:48:10 +0200

    loaded and transformed dataset
    added helper functions for dataset visualization and directory naming standardization

--- main.py
+++ main.py
@@ -1,52 +1,110 @@
 import os
-import urllib.request, json
+
 
 from typing import List, Dict, Tuple, Callable, Optional, Any
+
+import matplotlib.pyplot as plt
+import numpy as np
 import torch.cuda
 import torchvision.transforms
-from torchvision import datasets, transforms
-from torchvision.datasets.folder import default_loader
-from torch.utils.data import ConcatDataset
-
-# url = "https://storage.googleapis.com/kagglesdsdata/datasets/2310343/5229726/plant%20diseases%20cure/cure.json?X-Goog-Algorithm=GOOG4-RSA-SHA256&X-Goog-Credential=gcp-kaggle-com%40kaggle-161607.iam.gserviceaccount.com%2F20230415%2Fauto%2Fstorage%2Fgoog4_request&X-Goog-Date=20230415T155525Z&X-Goog-Expires=259200&X-Goog-SignedHeaders=host&X-Goog-Signature=67295bd32ad3e66d630bb4868c93a8bb339dbf65262984914ff01203c8ebbf986654895b5b6c41d891cce19f92e170daa3e731c12d97fb45a70189efc459c9b6e0f2abdf7cf7e58c577f119f3c61f776bc71b6e9617540a5d000d444e9594b2bc9ffd384607808f209ba86790bfafa26f6191c76ed5c33b3f43e622425291a8577ca58c1acd4bd260e94c521a0dff449005d2a03e1a54da019406c9b88ede78b083499f3ca3d3603c9eb5443f7657bbfdf140acac9ce005615d95e888c41851524058edad6fd09da37efea4d6fff48f06f28d293cfe27227aab95bee06d51185429331eed4788cc60768627eaba31af0c3f389d225cfcfedd501ad31874c2671"
-# with urllib.request.urlopen(url) as response:
-#     json_obj = json.load(response)
-#     json_obj_formatted = json.dumps(json_obj, indent=2)
-#     print(json_obj_formatted)
-
+from torchvision.datasets.folder import default_loader, DatasetFolder
+from torch.utils.data import random_split
+import re
+from torch.utils.data import DataLoader
+from util import rename_subdir
 
 def is_valid_file(filename: str) -> bool:
+    """Check if current file has valid extension """
+
     return filename.lower().endswith(('.jpg', '.png'))
 
 
 def find_classes(directory: str) -> Tuple[List[str], Dict[str, int]]:
-    """Finds the class folders in a dataset.
+    """Find the class folders in a dataset.
     See :class:`DatasetFolder` for details.
 
-    Change desired_classes list to load specific classes
+    Change the string inside regex search function to load plant specific classes
+
+    :param directory: Root directory of the training dataset
     """
-    classes = sorted(entry.name for entry in os.scandir(directory) if entry.is_dir())
+    classes = sorted(entry.name for entry in os.scandir(directory) if entry.is_dir()
+                     and re.search('apple', entry.name))
     if not classes:
         raise FileNotFoundError(f"Couldn't find any class folder in {directory}.")
-
-    desired_classes = ["apple_black_rot", "apple_cedar_rust", "apple_healthy", "apple_scab"]
-    classes = [cls_name for cls_name in classes if cls_name in desired_classes]
 
     class_to_idx = {cls_name: i for i, cls_name in enumerate(classes)}
     return classes, class_to_idx
 
 
 class CustomImageFolder(torchvision.datasets.DatasetFolder):
-    """ Implementing custom ImageFolder class that overrides DatasetFolder methods, so it's possible to load only
+    """ Implements custom ImageFolder class that overrides DatasetFolder methods, so it's possible to load only
     specific subdirectories(classes) of the directory instead of the whole directory.
 
-    Enabled two valid extensions (.jpg, .png)
+    Enables two valid extensions (.jpg, .png)
     """
     def __init__(self, root, transform=None, target_transform=None, loader=default_loader, is_valid_file=None):
         super().__init__(root, transform=transform, target_transform=target_transform, loader=loader,
                          extensions=('.jpg', '.png'), is_valid_file=is_valid_file)
     def find_classes(self, directory: str) -> Tuple[List[str], Dict[str, int]]:
         return find_classes(directory)
+
+
+def show_dataset(dataset: DatasetFolder, n=6) -> None:
+    """Show grid of images as a single image
+
+    :param dataset: Loaded torchvision dataset
+    :param n: Number of rows and columns
+    """
+
+    # Transform image from tensor to PILImage
+    transform = torchvision.transforms.ToPILImage()
+    img = np.vstack([np.hstack([np.asarray(transform(dataset[i][0])) for _ in range(n)])
+                     for i in range(n)])
+    plt.imshow(img)
+    plt.axis('off')
+    plt.show()
+
+
+def show_images(dataset_loader: DataLoader , num_of_images: int) -> None:
+    """Display images before feeding them to the model
+
+    Throws AssertionError if number of images to display exceeds batch size
+    """
+
+    batch_size = dataset_loader.batch_size
+
+    try:
+        assert num_of_images < batch_size,\
+            f"Number of images to display exceeds batch size: {num_of_images} > {batch_size}"
+
+        data_iter = iter(dataset_loader)
+        images, labels = next(data_iter)
+
+        figure = plt.figure()
+        for index in range(1, num_of_images + 1):
+            plt.subplot(2, 10, index)
+            plt.axis('off')
+            plt.title(str(labels.numpy()[index]))
+
+            # imshow function expects an image with the shape (height, width, channels)
+            img = images[index].numpy().transpose((1, 2, 0)).squeeze()
+            # img = (img - np.min(img)) / (np.max(img) - np.min(img))
+            plt.imshow(img)
+        plt.show()
+    except AssertionError as msg:
+        print("Error:", msg)
+
+
+def loader_shape(dataset_loader: DataLoader) -> Tuple[torch.Size, torch.Size]:
+    """Print shape of loaded dataset
+
+    :return: Tuple of tensor shapes (images, labels)
+    """
+
+    data_iter = iter(dataset_loader)
+    images, labels = next(data_iter)
+
+    return images.shape, labels.shape
 
 
 def train():
@@ -56,16 +114,49 @@ def train():
 
     root_dir = "data/plant_dataset_original/plant_diseases_images"
 
-    # apple_classes = ["apple_black_rot", "apple_cedar_rust", "apple_healthy", "apple_scab"]
-    # sub_dirs = [os.path.join(root_dir, subdir) for subdir in apple_classes if os.path.isdir(os.path.join(root_dir, subdir))]
-
-    transformations = transforms.Compose([
-        torchvision.transforms.Resize((225, 225)),
+    dataset_transforms = torchvision.transforms.Compose([
+        torchvision.transforms.Resize((224, 224)),
         torchvision.transforms.ToTensor(),
         torchvision.transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
     ])
 
-    dataset = CustomImageFolder(root=root_dir, loader=default_loader, transform=transformations)
-    print(dataset)
+    dataset = CustomImageFolder(root=root_dir, loader=default_loader, transform=dataset_transforms)
+    show_dataset(dataset)
 
-train()
+    train_size = int(len(dataset) * 0.8)
+    val_size = int(len(dataset) * 0.1)
+    test_size = len(dataset) - train_size - val_size
+
+    # Split the dataset
+    train_dataset, val_dataset, test_dataset = random_split(dataset, [train_size, val_size, test_size])
+
+    print(f"Number of training samples: {len(train_dataset)}")
+    print(f"Number of validation samples: {len(val_dataset)}")
+    print(f"Number of test samples: {len(test_dataset)}")
+
+    train_transforms = torchvision.transforms.Compose([
+        torchvision.transforms.ColorJitter(hue=.05, saturation=.05),
+        torchvision.transforms.RandomHorizontalFlip(),
+        torchvision.transforms.RandomRotation(10, interpolation=torchvision.transforms.InterpolationMode.BILINEAR),
+        torchvision.transforms.RandomGrayscale(),
+        torchvision.transforms.Resize((224, 224)),
+        torchvision.transforms.ToTensor(),
+        torchvision.transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+    ])
+
+    train_dataset.dataset.transform = train_transforms
+    show_dataset(train_dataset)
+
+    batch_size = 32
+
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=batch_size, shuffle=True)
+    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
+
+    images_shape, labels_shape = loader_shape(train_loader)
+    print(f'Images shape: {images_shape}\nLabels shape: {labels_shape}')
+
+    show_images(train_loader, num_of_images=20)
+
+if __name__ == '__main__':
+    train()
---
+++ util.py
@@ -0,0 +1,25 @@
+import os
+
+def rename_subdir(root_dir: str) -> None:
+    """Rename subdirectories to standard naming convention: plant_*_disease_*"""
+    for dir_name in os.listdir(root_dir):
+        # Remove repetition of any word
+        dir_name_target = ' '.join(sorted(set(dir_name.lower().split()), key=dir_name.lower().split().index))
+
+        # Remove list of words
+        words_to_remove = ["leaf"]
+        for word in words_to_remove:
+            dir_name_target = dir_name_target.replace(f'_{word}_', '_')
+
+        # Replace spaces with underscores
+        dir_name_target = dir_name_target.replace(' ', '_')
+
+        # Rename the directory
+        os.rename(os.path.join(root_dir, dir_name), os.path.join(root_dir, dir_name_target))
+
+
+def print_subdir_name(root_dir: str) -> None:
+    """Print a list of subdirectories names"""
+
+    subdirs = [os.path.basename(x) for x in os.listdir(root_dir) if os.path.isdir(os.path.join(root_dir, x))]
+    print(subdirs)
