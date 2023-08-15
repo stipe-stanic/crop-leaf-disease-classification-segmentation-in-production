40c0e89cec066de5de74e6bf751f53fccfe46e4f
Stipe Stanic <stipe.stanic156@gmail.com>
2023-08-15 23:02:13 +0200

    updated images for inference

--- deployment/docker/app/config.py
+++ deployment/docker/app/config.py
@@ -1,5 +1,5 @@
 class_names = ['apple_black_rot', 'apple_cedar_rust', 'apple_healthy', 'apple_scab', 'cherry_healthy',
-               'cherry_powdery_mildew', 'corn_blight', 'corn_common_rust', 'corn_gray_spot', 'corn_healthy',
+               'cherry_powdery_mildew', 'corn_blight', 'corn_common_rust', 'corn_healthy',
                'grape_black_measles', 'grape_black_rot', 'grape_blight_isariopsis_spot', 'grape_healthy',
                'orange_haunglongbing_citrus_greening', 'peach_bacterial_spot', 'peach_healthy',
                'pepper_bell_bacterial_spot', 'pepper_bell_healthy', 'potato_early_blight', 'potato_healthy',
